58de31a4aa6fb69df40e274b3b2a353950c02b2e
Sean Doyle <sean.p.doyle24@gmail.com>
2020-06-26 15:04:38 -0400

    simplify proxy

--- lib/action_client/test/web_mock_proxy.rb
+++ lib/action_client/test/web_mock_proxy.rb
@@ -3,68 +3,56 @@ module ActionClient
     class WebMockProxy < SimpleDelegator
       def self.stub_request(request)
         request_method = request.method.downcase.to_sym
+        request_body = request.body.read
 
-        webmock_chain = WebMock::API.stub_request(request_method, request.url)
+        webmock = WebMock::API.stub_request(request_method, request.url)
 
-        new(
-          action_name: request.client.action_name,
-          arguments: request.client.action_arguments,
-          request: request,
-          webmock_chain: webmock_chain
-        ).with(request: request)
+        if request_body.present?
+          webmock.with(body: request_body)
+        end
+
+        new(webmock, request)
       end
 
-      def initialize(action_name:, arguments:, request:, webmock_chain:)
-        super(webmock_chain)
-        @arguments = arguments
-        @action_name = action_name
+      def initialize(webmock, request)
+        super(webmock)
         @request = request
       end
 
-      def with(request: nil, **options, &block)
-        if request
-          options[:body] = request.body.read
-        end
-
-        chain(webmock.with(**options, &block))
-      end
-
       def to_return(*arguments, **options, &block)
-        if (mime_type = @request.headers["Accept"])
+        if (mime_type = request.headers["Accept"])
           options[:headers] ||= {}
           options[:headers].with_defaults!(Rack::CONTENT_TYPE => mime_type)
         end
 
-        chain(webmock.to_return(*arguments, **options, &block))
+        webmock.to_return(*arguments, **options, &block)
+
+        self
       end
 
       def to_fixture(locals: {}, status: 200, **options)
-        client = ActionClient::Test::Client.new(@request, status)
+        client = request.client
+        fixture_client = ActionClient::Test::Client.new(request, status)
 
-        client.process(@request.client.action_name, *@arguments, **locals)
+        fixture_client.process(client.action_name, *client.action_arguments, **locals)
 
-        status, headers, body = *client.response
+        status, headers, body = *fixture_client.response
 
         options[:status] = status
         options[:body] = body
         options[:headers] ||= {}
         options[:headers].with_defaults!(headers)
 
-        chain(webmock.to_return(options))
+        webmock.to_return(options)
+
+        self
       end
 
       private
 
-      alias webmock __getobj__
+      attr_reader :request
 
-      def chain(webmock_chain)
-        WebMockProxy.new(
-          arguments: @arguments,
-          request: @request,
-          webmock_chain: webmock_chain,
-          action_name: @action_name
-        )
-      end
+      alias webmock __getobj__
     end
   end
 end
